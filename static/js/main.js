1406b8c4ec79855b512a3d055b2e7b7c5a500153
sdl60660 <learnersd@gmail.com>
2020-05-18 17:25:35 -0400

    Added jquery ui touch punch for slider, attempting something else for play button, need to test on mobile device

--- static/js/main.js
+++ static/js/main.js
@@ -30,7 +30,7 @@ var usProjection = geoAlbersUsaPR()
 
 
 $('.toggle-button')
-    .on("touchstart click", function() {
+    .on("tap click", function() {
 
         $('.' + $(this).attr("class").split(' ').slice(-1)[0])
             .prop('disabled', false)
@@ -76,7 +76,7 @@ $("#slider-div").slider({
 })
 
 $("#play-button")
-    .on("touchstart click", function() {
+    .on("tap click", function() {
         var button = $(this);
 
         if (button.text() == "▶") {
