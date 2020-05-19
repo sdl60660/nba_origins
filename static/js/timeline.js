b8c4fb195c704fd9da05383b5c2bc566e43654c2
sdl60660 <learnersd@gmail.com>
2020-05-19 14:45:57 -0400

    Cleared out or commented out old code and and unnecessary imports on the HTML file

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -13,7 +13,6 @@ Timeline.prototype.initVis = function() {
     vis.svg = d3.select(vis.parentElement)
                 .append("svg")
                 .attr("preserveAspectRatio", "xMinYMin meet")
-				// .attr("viewBox", "0 0 960 500")
             	.attr("width", "105%")
             	.attr("height", "250%")
             	.attr("visibility", "visible");
@@ -30,26 +29,9 @@ Timeline.prototype.initVis = function() {
         			.tickFormat(d3.format("")))
             .attr('class', 'timeline-line')
             .attr("preserveAspectRatio", "xMinYMin meet")
-				// .attr("viewBox", "0 0 960 500")
         	.attr("width", "100%")
         	.attr("height", "120%")
-            // .attr('x0', 0)
-            // .attr('x1', "100%")
-            // .attr('y0', 0)
-            // .attr('y1', 0)
             .attr("transform", "translate(0,5)")
             .attr('stroke', 'black')
-            // .attr('stroke-width', '2px')
-
-    // vis.svg
-    //     .append('line')
-    //         .attr('class', 'timeline-line')
-    //         .attr('x0', 0)
-    //         .attr('x1', "100%")
-    //         .attr('y0', 0)
-    //         .attr('y1', 0)
-    //         // .attr("transform", "translate(0,-1)")
-    //         .attr('stroke', 'black')
-    //         .attr('stroke-width', '2px')
 
 }
